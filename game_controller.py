d21191ceddb4d37f7e3ba016dd7b245bb2ff8d29
Samsu-F <92742829+Samsu-F@users.noreply.github.com>
2024-01-03 06:37:01 +0100

    refactor

--- game_controller.py
+++ game_controller.py
@@ -6,12 +6,12 @@ def start_new(gridle):
     return
 
 def execute_swaps(gridle, swaps):
-    for start, end in swaps:
-        start = gridle.coordinates[start]
-        end = gridle.coordinates[end]
-        start = _get_cell_centre(start)
-        end = _get_cell_centre(end)
-        _adb_swipe(start, end)
+    for start_index, end_index in swaps:
+        start_cell = gridle.coordinates[start_index]
+        end_cell = gridle.coordinates[end_index]
+        start_coords = _get_cell_centre(start_cell)
+        end_coords = _get_cell_centre(end_cell)
+        _adb_swipe(start_coords, end_coords)
 
 def _adb_swipe(start, end, duration_ms=100):
     subprocess.run(["adb", "shell", "input", "touchscreen", "swipe", *map(str, start + end), str(duration_ms)])
