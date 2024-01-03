6109158a3eb6d53885bf52312031b7c5c288803e
Samsu-F <92742829+Samsu-F@users.noreply.github.com>
2024-01-03 03:16:08 +0100

    execute swaps with adb, implement function for pretty printing

--- main.py
+++ main.py
@@ -1,28 +1,34 @@
 import time
-import pyautogui
+# import pyautogui
+import subprocess
 from gridle_parser import parse_gridle
 from gridle_solver import solve
 from swap_solver import calculate_swaps
 from corpora import CORPUS_EN
+from print_gridle import print_gridle
 
 
-
-OFFSET_X = 35
-OFFSET_Y = 345
-SPACING_X = 78.5
-SPACING_Y = 73
+OFFSET_X = 154
+OFFSET_Y = 680
+SPACING = 177.5
 
 
 
 def row_col_to_pixel_coords(r, c):
-    return round(OFFSET_X + c * SPACING_X), round(OFFSET_Y + r * SPACING_Y)
+    return round(OFFSET_X + c * SPACING), round(OFFSET_Y + r * SPACING)
 
 
 
-def click_refresh_button():
-    pyautogui.mouseDown(321, 163)
-    pyautogui.mouseUp()
-    return
+# def click_refresh_button():
+#     pyautogui.mouseDown(321, 163)
+#     pyautogui.mouseUp()
+#     return
+
+
+
+def adb_swipe(start_x, start_y, end_x, end_y, duration_ms=100):
+    cmd = f"adb shell input touchscreen swipe {start_x} {start_y} {end_x} {end_y} {duration_ms}"
+    subprocess.run(cmd.split())
 
 
 
@@ -30,57 +36,50 @@ def execute_swaps(swaps):
     for (start_r, start_c), (end_r, end_c) in swaps:
         start_x, start_y = row_col_to_pixel_coords(start_r, start_c)
         end_x, end_y = row_col_to_pixel_coords(end_r, end_c)
-
-        # drag
-        pyautogui.mouseDown(start_x + 50, start_y + 50)
-        pyautogui.moveTo(end_x + 50, end_y + 50, duration=0.01)
-        pyautogui.mouseUp()
+        adb_swipe(start_x, start_y, end_x, end_y)
 
 
 
 def main():
-    while True:
-        click_refresh_button()
 
+    start_time = time.time()
+    gridle = parse_gridle()
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+
+    chars, colors = gridle
+    print_gridle(gridle)
+
+    print(f"parsing took {elapsed_time:.6f} seconds.")
+
+    try:
         start_time = time.time()
-        gridle = parse_gridle()
+        solution = solve(gridle, CORPUS_EN)
         end_time = time.time()
         elapsed_time = end_time - start_time
+        print(f"solving took {elapsed_time:.6f} seconds.")
+    except:
+        print("An error occurred while trying to solve.")
+        exit(1)
 
-        chars, colors = gridle
-        print("\n".join([str(x) for x in chars]))
-        print("\n".join([str(x) for x in colors]))
-
-        print(f"\tparsing took {elapsed_time:.6f} seconds.")
-
-        try:
-            start_time = time.time()
-            solution = solve(gridle, CORPUS_EN)
-            end_time = time.time()
-            elapsed_time = end_time - start_time
-            print(f"\tsolving took {elapsed_time:.6f} seconds.")
-        except:
-            print("An error occurred while trying to solve, skip.")
-            continue
-
-        solved = True
-        for r in range(5):
-            for c in range(5):
-                if (r in (0,2,4) or c in (0,2,4)) and solution[r][c] == None:
-                    solved = False
-        if not solved:
-            print("Could not solve, skip.")
-            continue
+    solved = True
+    for r in range(5):
+        for c in range(5):
+            if (r in (0,2,4) or c in (0,2,4)) and solution[r][c] == None:
+                solved = False
+    if not solved:
+        print("Could not solve.")
+        exit(1)
 
 
-        start_time = time.time()
-        swaps = calculate_swaps(chars, solution)
-        end_time = time.time()
-        elapsed_time = end_time - start_time
-        print(f"\tcalculating swaps took {elapsed_time:.6f} seconds.")
-        execute_swaps(swaps)
-        print(f"Solved with {len(swaps)} swaps.")
-        time.sleep(1.9)
+    start_time = time.time()
+    swaps = calculate_swaps(chars, solution)
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print(f"calculating swaps took {elapsed_time:.6f} seconds.")
+    execute_swaps(swaps)
+    print(f"Solved with {len(swaps)} swaps.")
+    # time.sleep(1.9)
 
 
 
